362dbf075a5752a2a88d0806a4d2669572a855a2
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-22 12:04:35 +0200

    initial static design

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,18 @@
-import { Fragment } from 'react'
-
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center'>
-      <div className='text-xl font-bold'>devfinder</div>
-      <div className='flex space-x-4 items-center'>
-        <p>Dark</p>
-        <img src="/img/icon-moon.svg" alt="" className='aspect-square h-5'/>
+    <nav className='flex justify-between items-center mb-12'>
+      <div className='text-xl font-bold dark:text-dm-color2'>devfinder</div>
+      <div className='flex space-x-2 items-center'>
+        <p className='font-bold text-xs tracking-widest dark:text-dm-color2'>DARK</p>
+        <svg className='fill-dm-color2' width='20' height='20' xmlns='http://www.w3.org/2000/svg'>
+          <path
+            d='M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z'
+          />
+        </svg>
+        {/* <img src="/img/icon-moon.svg" alt="" className='aspect-square h-5'/> */}
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ components/Search.js
@@ -0,0 +1,36 @@
+const Search = () => {
+  return (
+    <section>
+      <form className='h-[60px] bg-lm-color6 rounded-lg flex justify-around items-center px-2 text-xs w-full shadow-lg space-x-1 dark:bg-dm-color4 '>
+        {/* Icon */}
+        <svg
+          className='fill-lm-color1'
+          height='25'
+          width='25'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path d='M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z' />
+        </svg>
+        {/* Input */}
+        <input
+          className='w-8/12 text-lm-color3 placeholder:text-lm-color3 dark:bg-dm-color4 dark:placeholder:text-dm-color2 dark:text-dm-color2 md:w-6/12 md:text-base'
+          type='text'
+          placeholder='Search GitHub username...'
+        />
+        {/* Error */}
+        <span className='text-xxs text-err-color font-bold w-[2/12] whitespace-nowrap md:text-xs'>
+          No results
+        </span>
+        {/* Search */}
+        <button
+          className='bg-lm-color1 p-2.5 rounded-lg font-bold text-dm-color2 md:px-5 md:text-base'
+          type='submit'
+        >
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Search;
